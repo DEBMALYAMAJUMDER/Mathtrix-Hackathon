3c1cd40626aba3c82018241a539a5ac5aa42ac9b
Debmalya <debmalyamajumder91@gmail.com>
2025-11-10 22:22:37 +0530

    Added BranchName enum

--- src/main/java/org/mathtrix/hackathon/validation/validator/BranchValidator.java
+++ src/main/java/org/mathtrix/hackathon/validation/validator/BranchValidator.java
@@ -2,18 +2,19 @@ package org.mathtrix.hackathon.validation.validator;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.mathtrix.hackathon.entity.BranchName;
 import org.mathtrix.hackathon.validation.ValidateBranch;
 
 import java.util.Set;
 
 public class BranchValidator implements ConstraintValidator<ValidateBranch, String> {
-    private static final Set<String> ALLOWED_BRANCHES = Set.of("main", "master");
+    private static final Set<String> ALLOWED_BRANCHES = Set.of(BranchName.MASTER.name(), BranchName.MAIN.name());
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null || value.isBlank()) {
             return false;
         }
-        return ALLOWED_BRANCHES.contains(value.trim());
+        return ALLOWED_BRANCHES.contains(value.trim().toUpperCase());
     }
 }
